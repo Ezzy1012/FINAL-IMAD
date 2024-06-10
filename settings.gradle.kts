eb92373dc75a24e00a9ea7dc856bdbfe6ca13e32
Ezzy1012 <161321071+Ezzy1012@users.noreply.github.com>
2024-06-10 15:13:31 +0200

    Add files via upload

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Summative_Project_IMAD"
+include(":app")
+ 
